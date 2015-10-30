eb5c4ce5e7a69c06cfcf2588a9abed4542f924cc
Cécile Dreyfus <cecile.dreyfuslaquieze@gmail.com>
2015-10-30 14:05:19 +0100

    "déconnexion" et js d'authent

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('ent', ['ionic', 'ent.controllers'])
+angular.module('ent', ['ionic', 'ent.controllers', 'ent.auth'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/auth.js
+++ www/js/auth.js
@@ -1,1 +1,11 @@
 angular.module('ent.auth', [])
+
+
+
+.controller('LoginCtrl', function($scope, $state) {
+
+  $scope.doLogin = function(user) {
+    $state.go('app.playlists');
+  };
+
+})
