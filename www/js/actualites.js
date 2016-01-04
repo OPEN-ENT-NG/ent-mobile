0165fe2a60089facc03d961ba1655bffb1d4f705
Cécile Dreyfus <cecile.dreyfus@atos.net>
2016-01-04 13:32:50 +0100

    ajout d'un pull to refresh

--- www/js/actualites.js
+++ www/js/actualites.js
@@ -1,7 +1,7 @@
 angular.module('ent.actualites', [])
 
 .controller('InfosCtrl', function ($scope, $http,$ionicPopover, $state, $rootScope) {
-    $http.get("https://recette-leo.entcore.org/actualites/infos").then(function(resp){
+  $http.get("https://recette-leo.entcore.org/actualites/infos").then(function(resp){
     $scope.infos = [];
     for(var i = 0; i< resp.data.length; i++){
       $scope.infos.push({
@@ -47,86 +47,115 @@ angular.module('ent.actualites', [])
     return size+" "+unite;
   }
 
-    $http.get("https://recette-leo.entcore.org/actualites/threads").then(function(resp){
-      $scope.threads = [];
+  $http.get("https://recette-leo.entcore.org/actualites/threads").then(function(resp){
+    $scope.threads = [];
+    for(var i = 0; i< resp.data.length; i++){
+      $scope.threads.push({
+        thread_id: resp.data[i]._id,
+        title: resp.data[i].title,
+        thread_icon: resp.data[i].icon
+      });
+    }
+  }, function(err){
+    alert('ERR:'+ err);
+  });
+
+
+  // $scope.threads = [
+  //   {title: "Brouillons", thread_id: 10},
+  //   {title: "Soumises", thread_id: 27},
+  //   {title: "Publiées", thread_id: 31}
+  // ];
+  /*
+  * if given group is the selected group, deselect it
+  * else, select the given group
+  */
+  $scope.toggleComments = function(info) {
+    console.log(info.comments);
+    if ($scope.areCommentsShown(info)) {
+      $scope.shownComments = null;
+    } else {
+      $scope.shownComments = info;
+    }
+  };
+  $scope.areCommentsShown = function(info) {
+    return $scope.shownComments === info;
+  };
+
+  $scope.goThreads = function(){
+    $state.go("app.threads");
+  };
+
+  $scope.statusInfos = [
+    {nom: "Brouillons", status: 1},
+    {nom: "Soumises", status: 2},
+    {nom: "Publiées", status: 3}
+  ];
+
+  $rootScope.filterInfos = [];
+  $scope.filterByStatus = function (state) {
+    return $rootScope.filterInfos[state.status] | noFilter($rootScope.filterInfos);
+  };
+
+  $rootScope.filterThreads = [];
+  $scope.filterByThread = function (thread) {
+    return $rootScope.filterThreads[thread.thread_id]| noFilter($rootScope.filterThreads);
+  };
+
+  function noFilter(filterObj) {
+    for (var key in filterObj) {
+      if (filterObj[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  $scope.doRefresh = function() {
+
+    $http.get("https://recette-leo.entcore.org/actualites/infos").then(function(resp){
+      $scope.infos = [];
       for(var i = 0; i< resp.data.length; i++){
-        $scope.threads.push({
-          thread_id: resp.data[i]._id,
+        $scope.infos.push({
+          _id: resp.data[i]._id,
           title: resp.data[i].title,
-          thread_icon: resp.data[i].icon
+          content: resp.data[i].content,
+          status: resp.data[i].status,
+          publication_date: resp.data[i].publication_date,
+          thread_id: resp.data[i].thread_id,
+          username: resp.data[i].username,
+          thread_icon: resp.data[i].thread_icon,
+          comments: angular.fromJson(resp.data[i].comments)
         });
       }
     }, function(err){
       alert('ERR:'+ err);
-    });
-
-
-    // $scope.threads = [
-    //   {title: "Brouillons", thread_id: 10},
-    //   {title: "Soumises", thread_id: 27},
-    //   {title: "Publiées", thread_id: 31}
-    // ];
-    /*
-    * if given group is the selected group, deselect it
-    * else, select the given group
-    */
-    $scope.toggleComments = function(info) {
-      console.log(info.comments);
-      if ($scope.areCommentsShown(info)) {
-        $scope.shownComments = null;
-      } else {
-        $scope.shownComments = info;
-      }
-    };
-    $scope.areCommentsShown = function(info) {
-      return $scope.shownComments === info;
-    };
-
-    $scope.goThreads = function(){
-      $state.go("app.threads");
-    };
-
-    $scope.statusInfos = [
-      {nom: "Brouillons", status: 1},
-      {nom: "Soumises", status: 2},
-      {nom: "Publiées", status: 3}
-    ];
-
-    $rootScope.filterInfos = [];
-    $scope.filterByStatus = function (state) {
-      return $rootScope.filterInfos[state.status] | noFilter($rootScope.filterInfos);
-    };
-
-    $rootScope.filterThreads = [];
-    $scope.filterByThread = function (thread) {
-        return $rootScope.filterThreads[thread.thread_id]| noFilter($rootScope.filterThreads);
-    };
-
-    function noFilter(filterObj) {
-      for (var key in filterObj) {
-        if (filterObj[key]) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    $ionicPopover.fromTemplateUrl('templates/popover_actualites.html', {
-      scope: $scope
-    }).then(function(popover) {
-      $scope.popover = popover;
-    });
-
-    $scope.openPopover = function($event) {
-      $scope.popover.show($event);
-    };
-
-    $scope.closePopover = function() {
-      $scope.popover.hide();
-    };
-
-    //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.popover.remove();
     })
+    .finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });
+
+    $scope.$broadcast('scroll.refreshComplete');
+    $scope.$apply()
+  };
+
+  $ionicPopover.fromTemplateUrl('templates/popover_actualites.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
   });
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  })
+});
