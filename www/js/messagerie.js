b4559418e40e28054bb388fc9423617ad27216c6
Cécile Dreyfus <cecile.dreyfus@atos.net>
2016-01-22 17:41:32 +0100

    rédaction d'un mail en cours

--- www/js/messagerie.js
+++ www/js/messagerie.js
@@ -8,27 +8,27 @@ angular.module('ent.messagerie', [])
   });
 
   $rootScope.contacts = [];
-  $http.get("https://recette-leo.entcore.org/conversation/visible").then(function(resp){
-
-    for(var i = 0; i< resp.data.groups.length; i++){
-      $rootScope.contacts.push({
-        _id:  resp.data.groups[i].id,
-        displayName:  resp.data.groups[i].name,
-        groupDisplayName:  resp.data.groups[i].groupDisplayName,
-        profile:  resp.data.groups[i].status
-      });
-    }
-    for(var i = 0; i<  $rootScope.otherContacts.users.length; i++){
-      $rootScope.contacts.push({
-        _id:  resp.data.users[i].id,
-        displayName:  resp.data.users[i].displayName,
-        groupDisplayName:  resp.data.users[i].groupDisplayName,
-        profile:  resp.data.users[i].status
-      });
-    };
-  }, function(err){
-    alert('ERR:'+ err);
-  });
+  // $http.get("https://recette-leo.entcore.org/conversation/visible").then(function(resp){
+  //
+  //   for(var i = 0; i< resp.data.groups.length; i++){
+  //     $rootScope.contacts.push({
+  //       _id:  resp.data.groups[i].id,
+  //       displayName:  resp.data.groups[i].name,
+  //       groupDisplayName:  resp.data.groups[i].groupDisplayName,
+  //       profile:  resp.data.groups[i].status
+  //     });
+  //   }
+  //   for(var i = 0; i<  $rootScope.otherContacts.users.length; i++){
+  //     $rootScope.contacts.push({
+  //       _id:  resp.data.users[i].id,
+  //       displayName:  resp.data.users[i].displayName,
+  //       groupDisplayName:  resp.data.users[i].groupDisplayName,
+  //       profile:  resp.data.users[i].status
+  //     });
+  //   };
+  // }, function(err){
+  //   alert('ERR:'+ err);
+  // });
 
 
   $scope.setCurrentFolder = function (index){
@@ -70,83 +70,80 @@ angular.module('ent.messagerie', [])
   // ];
 
 
-  // $rootScope.otherContacts =
-  // {
-  //
-  //   groups:[
-  //     {
-  //       id: "73825-1452590663199",
-  //       name: "Tous les gestionnnaires de la communauté a.",
-  //       groupDisplayName: null,
-  //       profile: null
-  //
-  //     },
-  //     {
-  //
-  //       id: "73786-1452526764539",
-  //       name: "Tous les gestionnnaires de la communauté test-forum.",
-  //       groupDisplayName: null,
-  //       profile: null
-  //
-  //     },
-  //     {
-  //
-  //       id: "25371-1452097565207",
-  //       name: "Tous les gestionnnaires de la communauté Test 4855.",
-  //       groupDisplayName: null,
-  //       profile: null
-  //     }
-  //   ],
-  //   users: [
-  //     {
-  //
-  //       id: "8ba1eaac-28f0-41e4-b38f-d20ce4d9a2ba",
-  //       displayName: "Aurélie DROUILLAC",
-  //       groupDisplayName: null,
-  //       profile: "Teacher"
-  //
-  //     },
-  //     {
-  //
-  //       id: "91d042ed-b4d5-4dc8-bd97-fb0360c01f2b",
-  //       displayName: "VINCENT CAILLET",
-  //       groupDisplayName: null,
-  //       profile: "Teacher"
-  //
-  //     },
-  //     {
-  //
-  //       id: "19fee7e7-c10a-4730-a987-71931aab7199",
-  //       displayName: "Andréa Ross",
-  //       groupDisplayName: null,
-  //       profile: "Student"
-  //
-  //     }
-  //   ]
-  // };
+  $rootScope.otherContacts =
+  {
 
-  // $rootScope.transform = function(){
-  //   for(var i = 0; i< $rootScope.otherContacts.groups.length; i++){
-  //     $rootScope.contacts.push({
-  //       _id:  $rootScope.otherContacts.groups[i].id,
-  //       displayName:  $rootScope.otherContacts.groups[i].name,
-  //       groupDisplayName:  $rootScope.otherContacts.groups[i].groupDisplayName,
-  //       profile:  $rootScope.otherContacts.groups[i].profile
-  //     });
-  //   }
-  //   for(var i = 0; i<  $rootScope.otherContacts.users.length; i++){
-  //     $rootScope.contacts.push({
-  //       _id:  $rootScope.otherContacts.users[i].id,
-  //       displayName:  $rootScope.otherContacts.users[i].displayName,
-  //       groupDisplayName:  $rootScope.otherContacts.users[i].groupDisplayName,
-  //       profile:  $rootScope.otherContacts.users[i].profile
-  //     });
-  //   };
-  //   for(var i = 0; i<  $rootScope.contacts.length; i++){
-  //     console.log( $rootScope.contacts[i]);
-  //
-  //   }
-  // }
+    groups:[
+      {
+        id: "73825-1452590663199",
+        name: "Tous les gestionnnaires de la communauté a.",
+        groupDisplayName: null,
+        profile: null
+
+      },
+      {
+
+        id: "73786-1452526764539",
+        name: "Tous les gestionnnaires de la communauté test-forum.",
+        groupDisplayName: null,
+        profile: null
+
+      },
+      {
+
+        id: "25371-1452097565207",
+        name: "Tous les gestionnnaires de la communauté Test 4855.",
+        groupDisplayName: null,
+        profile: null
+      }
+    ],
+    users: [
+      {
+
+        id: "8ba1eaac-28f0-41e4-b38f-d20ce4d9a2ba",
+        displayName: "Aurélie DROUILLAC",
+        groupDisplayName: null,
+        profile: "Teacher"
+
+      },
+      {
+
+        id: "91d042ed-b4d5-4dc8-bd97-fb0360c01f2b",
+        displayName: "VINCENT CAILLET",
+        groupDisplayName: null,
+        profile: "Teacher"
+
+      },
+      {
+
+        id: "19fee7e7-c10a-4730-a987-71931aab7199",
+        displayName: "Andréa Ross",
+        groupDisplayName: null,
+        profile: "Student"
+
+      }
+    ]
+  };
+
+  $rootScope.transform = function(){
+    for(var i = 0; i< $rootScope.otherContacts.groups.length; i++){
+      $rootScope.contacts.push({
+        _id:  $rootScope.otherContacts.groups[i].id,
+        displayName:  $rootScope.otherContacts.groups[i].name,
+        groupDisplayName:  $rootScope.otherContacts.groups[i].groupDisplayName,
+        profile:  $rootScope.otherContacts.groups[i].profile
+      });
+    }
+    for(var i = 0; i<  $rootScope.otherContacts.users.length; i++){
+      $rootScope.contacts.push({
+        _id:  $rootScope.otherContacts.users[i].id,
+        displayName:  $rootScope.otherContacts.users[i].displayName,
+        groupDisplayName:  $rootScope.otherContacts.users[i].groupDisplayName,
+        profile:  $rootScope.otherContacts.users[i].profile
+      });
+    };
+  
+  }
 
 
 })
--- www/js/new_message.js
+++ www/js/new_message.js
@@ -1,8 +1,134 @@
 angular.module('ent.new_message', [])
 
-.controller('NewMessageCtrl', function($scope, $http, $rootScope){
+.controller('NewMessageCtrl', function($scope, $http, $rootScope, $ionicPopover, $state, $ionicHistory){
 
-  $scope.destinataires = [];
+  $scope.destinatairesTo = [];
+  $scope.destinatairesCc = [];
+
+  $scope.addContactTo = function(search, contact){
+    $scope.destinatairesTo.push(contact);
+    search.value ="";
+  }
+
+  $scope.addContactCc = function(search, contact){
+    $scope.destinatairesCc.push(contact);
+    search.value ="";
+  }
+
+  $scope.deleteFromDestinataireTo = function(destinataire){
+    var index = $scope.destinatairesTo.indexOf(destinataire);
+    $scope.destinatairesTo.splice(index, 1);
+  }
+
+  $scope.deleteFromDestinataireCc = function(destinataire){
+    var index = $scope.deleteFromDestinataireCc.indexOf(destinataire);
+    $scope.destinatairesCc.splice(index, 1);
+  }
+
+  $scope.goToMessagerie = function () {
+    $scope.closePopover();
+    $ionicHistory.goBack(-1);
+  }
+
+  $ionicPopover.fromTemplateUrl('templates/popover_messagerie.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  })
+
+
+  $rootScope.contacts = [];
+  $rootScope.otherContacts =
+  {
+
+    groups:[
+      {
+        id: "73825-1452590663199",
+        name: "Tous les gestionnnaires de la communauté a.",
+        groupDisplayName: null,
+        profile: null
+
+      },
+      {
+
+        id: "73786-1452526764539",
+        name: "Tous les gestionnnaires de la communauté test-forum.",
+        groupDisplayName: null,
+        profile: null
+
+      },
+      {
+
+        id: "25371-1452097565207",
+        name: "Tous les gestionnnaires de la communauté Test 4855.",
+        groupDisplayName: null,
+        profile: null
+      }
+    ],
+    users: [
+      {
+
+        id: "8ba1eaac-28f0-41e4-b38f-d20ce4d9a2ba",
+        displayName: "Aurélie DROUILLAC",
+        groupDisplayName: null,
+        profile: "Teacher"
+
+      },
+      {
+
+        id: "91d042ed-b4d5-4dc8-bd97-fb0360c01f2b",
+        displayName: "VINCENT CAILLET",
+        groupDisplayName: null,
+        profile: "Teacher"
+
+      },
+      {
+
+        id: "19fee7e7-c10a-4730-a987-71931aab7199",
+        displayName: "Andréa Ross",
+        groupDisplayName: null,
+        profile: "Student"
+
+      }
+    ]
+  };
+
+  $rootScope.transform = function(){
+    for(var i = 0; i< $rootScope.otherContacts.groups.length; i++){
+      $rootScope.contacts.push({
+        _id:  $rootScope.otherContacts.groups[i].id,
+        displayName:  $rootScope.otherContacts.groups[i].name,
+        groupDisplayName:  $rootScope.otherContacts.groups[i].groupDisplayName,
+        profile:  $rootScope.otherContacts.groups[i].profile
+      });
+    }
+    for(var i = 0; i<  $rootScope.otherContacts.users.length; i++){
+      $rootScope.contacts.push({
+        _id:  $rootScope.otherContacts.users[i].id,
+        displayName:  $rootScope.otherContacts.users[i].displayName,
+        groupDisplayName:  $rootScope.otherContacts.users[i].groupDisplayName,
+        profile:  $rootScope.otherContacts.users[i].profile
+      });
+    };
+    for(var i = 0; i <  $rootScope.contacts.length; i++){
+      console.log( $rootScope.contacts[i]);
+
+    }
+  }
+
 
 
 })
@@ -36,14 +162,8 @@ angular.module('ent.new_message', [])
         scope.search.value = '';
       };
     },
-    template: ' <div id="filter-box" class="item item-input-inset">' +
-    '<div class="item-input-wrapper">' +
-    '<i class="icon ion-android-search"></i>' +
-    '<input style="font-size: 1.2rem;" type="search" placeholder="{{placeholder}}" ng-model="search.value">' +
-    '</div>' +
-
-    '<button class="button button-clear ion-close" style="color: #333" ng-if="search.value.length > 0" ng-click="clearSearch()">' +
-    '</button>' +
+    template: ' <ion-input fixed-label id="filter-box" class=" item-input"><ion-label>{{myLabel}}</ion-label>' +
+    '<input type="search" ng-model="search.value">' +
     '</div>'
   };
 })
