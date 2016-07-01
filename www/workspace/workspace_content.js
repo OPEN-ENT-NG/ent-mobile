90beb6a5ccea305cf5a0c7235c3ae89dc8cda8eb
Thomas Perrin <thomas.perrin@atos.net>
2016-07-01 16:30:44 +0200

    Correction d'une permission

--- www/workspace/workspace_content.js
+++ www/workspace/workspace_content.js
@@ -58,26 +58,6 @@ angular.module('ent.workspace_content',['ent.workspace_service',])
     }
   }
 
-  $rootScope.isIHaveRightToRename = function(){
-    if($scope.folders){
-      if($stateParams.nameWorkspaceFolder == "documents"){
-        return true;
-      } else if($stateParams.nameWorkspaceFolder == "shared"){
-        var foldChecked = getCheckedFolders($scope.folders);
-        for(var i = 0 ; i < foldChecked.length ; i++ ){
-          for(var j = 0 ; j < foldChecked[i].shared.length ; j++){
-            if(foldChecked[i].shared[j].userId == $rootScope.myUser.id){
-              if(foldChecked[i].shared[j]['org-entcore-workspace-service-WorkspaceService|updateDocument']){
-                return true ;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-
   /*
   * if given group is the selected group, deselect it
   * else, select the given group
