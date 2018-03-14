f471244a44577062d6f0a9bdf14da58d89693bbb
Quentin Rollin <quentin.rollin@cgi.com>
2018-03-14 16:25:05 +0100

    [AM-10][AM-8][AM-7] Image non chargees

--- www/lib/angular-img-http-src/index.js
+++ www/lib/angular-img-http-src/index.js
@@ -1,4 +1,4 @@
-/*(function () {
+(function () {
 	'use strict';
 
 	angular.module('angular.img', [
@@ -39,46 +39,45 @@
 		};
 	}]);
 }());
-*/
-(function() {
-    'use strict';
-
-    angular.module('angular.img', []).directive('httpSrc', [
-        '$http',
-        function($http) {
-            var directive = {
-                link: link,
-                restrict: 'A'
-            };
-            return directive;
-
-            function link(scope, element, attrs) {
-                var requestConfig = {
-                    method: 'Get',
-                    url: attrs.httpSrc,
-                    responseType: 'arraybuffer',
-                    cache: 'true'
-                };
-
-                $http(requestConfig)
-                    .then(function(res) {
-                        var data = res.data;
-                        var arr = new Uint8Array(data);
-
-                        var raw = '';
-                        var i, j, subArray, chunk = 5000;
-                        for (i = 0, j = arr.length; i < j; i += chunk) {
-                            subArray = arr.subarray(i, i + chunk);
-                            raw += String.fromCharCode.apply(null, subArray);
-                        }
-
-                        var b64 = btoa(raw);
-                        var minetype = res.headers('Content-Type');//image/png;
-                        var img = "data:"+minetype+";base64," + b64;
-                        attrs.$set('src', img);
-                    });
-            }
-
-        }
-    ]);
-}());
+// (function() {
+//     'use strict';
+//
+//     angular.module('angular.img', []).directive('httpSrc', [
+//         '$http',
+//         function($http) {
+//             var directive = {
+//                 link: link,
+//                 restrict: 'A'
+//             };
+//             return directive;
+//
+//             function link(scope, element, attrs) {
+//                 var requestConfig = {
+//                     method: 'Get',
+//                     url: attrs.httpSrc,
+//                     responseType: 'arraybuffer',
+//                     cache: 'true'
+//                 };
+//
+//                 $http(requestConfig)
+//                     .then(function(res) {
+//                         var data = res.data;
+//                         var arr = new Uint8Array(data);
+//
+//                         var raw = '';
+//                         var i, j, subArray, chunk = 5000;
+//                         for (i = 0, j = arr.length; i < j; i += chunk) {
+//                             subArray = arr.subarray(i, i + chunk);
+//                             raw += String.fromCharCode.apply(null, subArray);
+//                         }
+//
+//                         var b64 = btoa(raw);
+//                         var minetype = res.headers('Content-Type');//image/png;
+//                         var img = "data:"+minetype+";base64," + b64;
+//                         attrs.$set('src', img);
+//                     });
+//             }
+//
+//         }
+//     ]);
+// }());
