18a8db2536268b0867a28809a62ee6286c687442
Quentin Rollin <quentin.rollin@cgi.com>
2018-05-24 18:21:38 +0200

    [MEP MDP] Afficher authentification persistente

--- www/app.const.js
+++ www/app.const.js
@@ -1,13 +1,12 @@
 angular.module('ent')
-// .value("domainENT", "https://ent.picardie.fr")
+.value("domainENT", "https://ent.parisclassenumerique.fr")
 //  .value("domainENT", "https://preprod-leo.entcore.org")
- .value("domainENT", "https://preprod-paris.opendigitaleducation.com")
-// .value("domainENT", "http://10.83.199.153:8090")
-// .value("domainENT", "http://ent4-core.gdapublic.fr")
-  // .value("domainENT", "http://10.83.199.37:8090")
+//  .value("domainENT", "https://preprod-paris.opendigitaleducation.com")
+//  .value("domainENT", "http://ent4-core.gdapublic.fr")
+//  .value("domainENT", "https://cgi-u-dev.fr")
 //   .value("domainENT", "https://ent-core.gdapublic.fr")
-//   .value("domainENT", "http://localhost:8090")
   .value("OAuth2Params", {
     clientId : "Application Mobile", /* champ identifiant */
-    secret : "secret" /* champ code secret */
+   // secret : "secret" /* champ code secret */
+   secret : "mdpconnecteursecret" /* champ code secret */
   });
