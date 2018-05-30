a3fe8f1ab833928ea5617242b84480116685d1eb
Quentin Rollin <quentin.rollin@cgi.com>
2018-05-30 15:36:00 +0200

    [AM-59] Mise en place Pronote

--- www/pronotes/pronotes.js
+++ www/pronotes/pronotes.js
@@ -1,37 +1,52 @@
 angular.module('ent.pronotes', ['ent.pronotes_service'])
 
-  .controller('ListingUsersCtrl', function ($scope, $rootScope, $window, PronoteService, $ionicPlatform, $sce, $state, $ionicLoading) {
+  .controller('ListingUsersCtrl', function (domainENT, $scope, $rootScope, $window, PronoteService, $ionicPlatform,
+                                            $http, $sce, $state, $ionicLoading) {
     $scope.pronotes = [];
 
-    PronoteService.getAllApps().then(function(resp){
-      if(resp != null)
-        for(var i=0  ; i < resp.data.apps.length ; i++){
-          if(resp.data.apps[i].name.indexOf("Pronote") > -1){
+    $ionicPlatform.ready(function () {
+      $scope.$on('$ionicView.enter', function () {
+        PronoteService.getAllApps().then(function (resp) {
+          if (resp != null)
+            for (var i = 0; i < resp.data.apps.length; i++) {
+              if (resp.data.apps[i].name.indexOf("Pronote") > -1) {
+                $scope.pronotes.push({
+                  name: resp.data.apps[i].name,
+                  address: domainENT + "/oauth/login?service=" + btoa(resp.data.apps[i].address)
+                });
+              }
+            }
+          if ($scope.pronotes.length === 0) {
             $scope.pronotes.push({
-              name: resp.data.apps[i].name,
-              address: resp.data.apps[i].address
+              name: "test",
+              address: domainENT + "/cas/oauth/login?service=" + encodeURIComponent("https://extranet.clg-jean-baptiste-poquelin.ac-paris.fr/pronote/")
             });
           }
-        }
+        });
+      });
+
+      if ($state.current.name == "app.pronote") {
+        $ionicLoading.show({
+          template: '<ion-spinner icon="android"/>'
+        });
+
+        $http.get($rootScope.link, {
+          "Authorization": $http.defaults.headers.common['Authorization']
+        }).then(function success(response) {
+          $scope.link = $sce.trustAsResourceUrl(response.headers()['location']);
+
+          document.querySelector("#iframe").onload = function () {
+            $ionicLoading.hide()
+          };
+        }, function error(err) {
+          $ionicLoading.hide();
+        });
+      }
     });
 
-    if($state.current.name == "app.pronote"){
-      $ionicLoading.show({
-        template: '<ion-spinner icon="android"/>'
-      });
-      var iframe = document.querySelector( "#iframe" );
-      var iframeContent = iframe.contentDocument || iframe.contentWindow.document;
-      if (  iframeContent.readyState  == 'complete' ) {
-        iframe.onload = function(){
-            $ionicLoading.hide();
-        };
-      }
-    }
-
-    $scope.showPronote = function(link,namePronote){
+    $scope.showPronote = function (link, namePronote) {
       $rootScope.pronoteName = namePronote;
       $rootScope.link = $sce.trustAsResourceUrl(link);
       $state.go("app.pronote");
     }
-
   });
--- www/pronotes/pronotes_service.js
+++ www/pronotes/pronotes_service.js
@@ -4,9 +4,4 @@ angular.module('ent.pronotes_service', ['ent.request'])
   this.getAllApps = function(){
     return RequestService.get(domainENT+"/applications-list");
   }
-
-  // this.getTranslation = function(){
-  //   return $http.get(domainENT+'/applications-list/i18n');
-  // }
-
 });
