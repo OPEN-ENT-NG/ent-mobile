15ce9366124f28e030f28d75683913fbe90493ef
LEDUNOIS Simon <simon.ledunois@gmail.com>
2018-11-07 13:26:31 +0100

    AM-135 : Redirect to AuthLoading

--- www/requestService.js
+++ www/requestService.js
@@ -1,28 +1,26 @@
-angular.module('ent.request', ['ent.workspace_service', 'ent'])
+angular
+  .module("ent.request", ["ent.workspace_service", "ent"])
 
-  .service('RequestService', function($timeout, $http, $q, $state, $ionicLoading) {
-
-    // var timeout = $timeout(function () {
-    //     $ionicPopup.show({
-    //       title: 'Pas de connexion à Internet.',
-    //       buttons: [
-    //         {text: 'OK'}
-    //       ]
-    //     });
-    //   }, 10000);
+  .service("RequestService", function(
+    $timeout,
+    $http,
+    $q,
+    $state,
+    $ionicLoading
+  ) {
     var timeout = 3000;
 
-    function onError (reject, error) {
+    function onError(reject, error) {
       $ionicLoading.hide();
       reject(error);
     }
 
-    function onResolve (resolve, response) {
+    function onResolve(resolve, response) {
       var str = response.data.toString();
       if (str.startsWith("<!doctype html>")) {
         $ionicLoading.hide();
         resolve(response);
-        $state.go('login');
+        $state.go("login");
       } else {
         resolve(response);
       }
@@ -31,61 +29,79 @@ angular.module('ent.request', ['ent.workspace_service', 'ent'])
     function getConfig(config) {
       config = config || {};
       config.timeout = timeout;
-      config.headers  = Object.assign(config.headers || {}, $http.defaults.headers.common);
+      config.headers = Object.assign(
+        config.headers || {},
+        $http.defaults.headers.common
+      );
       return config;
     }
 
-    this.setDefaultHeaders = function () {
-      $http.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
-      $http.defaults.headers.common['Accept'] = 'application/json;charset=UTF-8';
-    }
+    this.setDefaultHeaders = function() {
+      $http.defaults.headers.common["Content-Type"] =
+        "application/x-www-form-urlencoded; charset=UTF-8";
+      $http.defaults.headers.common["Accept"] =
+        "application/json;charset=UTF-8";
+    };
 
-    this.setDefaultAuth = function (tokens) {
-      $http.defaults.headers.common['Authorization'] = 'Bearer ' + tokens.access;
+    this.setDefaultAuth = function(tokens) {
+      $http.defaults.headers.common["Authorization"] =
+        "Bearer " + tokens.access;
       return tokens;
     };
 
-    this.get = function (url, config) {
+    this.get = function(url, config) {
       config = getConfig(config);
-      return $q(function(resolve, reject){
-        $http.get(url, config).then(function (response) {
-          onResolve(resolve, response);
-        }, function (err) {
-          onError(reject, err);
-        });
+      return $q(function(resolve, reject) {
+        $http.get(url, config).then(
+          function(response) {
+            onResolve(resolve, response);
+          },
+          function(err) {
+            onError(reject, err);
+          }
+        );
       });
     };
 
-    this.delete = function (url, config) {
+    this.delete = function(url, config) {
       config = getConfig(config);
-      return $q(function(resolve, reject){
-        $http.delete(url, config).then(function (response) {
-          onResolve(resolve, response);
-        }, function (err) {
-          onError(reject, err);
-        });
+      return $q(function(resolve, reject) {
+        $http.delete(url, config).then(
+          function(response) {
+            onResolve(resolve, response);
+          },
+          function(err) {
+            onError(reject, err);
+          }
+        );
       });
     };
 
-    this.put = function (url, data, config) {
+    this.put = function(url, data, config) {
       config = getConfig(config);
-      return $q(function(resolve, reject){
-        $http.put(url, data, config).then(function (response) {
-          onResolve(resolve, response);
-        }, function (err) {
-          onError(reject, err);
-        });
+      return $q(function(resolve, reject) {
+        $http.put(url, data, config).then(
+          function(response) {
+            onResolve(resolve, response);
+          },
+          function(err) {
+            onError(reject, err);
+          }
+        );
       });
     };
 
-    this.post = function (url, data, config) {
+    this.post = function(url, data, config) {
       config = getConfig(config);
-      return $q(function(resolve, reject){
-        $http.post(url, data, config).then(function (response) {
-          onResolve(resolve, response);
-        }, function (err) {
-          onError(reject, err);
-        });
+      return $q(function(resolve, reject) {
+        $http.post(url, data, config).then(
+          function(response) {
+            onResolve(resolve, response);
+          },
+          function(err) {
+            onError(reject, err);
+          }
+        );
       });
     };
   });
