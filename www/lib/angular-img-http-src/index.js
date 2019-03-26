57fc592cb5c9ff8b57d36fc6bda2aa5959e61e44
ROLLIN Quentin <quentin.rollin@cgi.com>
2019-03-26 11:50:40 +0100

    Revert "Delete www/lib/ folder"
    This reverts commit 87a6825aa19d878ee9b67f94c0070bee89d5cba2.

---
+++ www/lib/angular-img-http-src/index.js
@@ -0,0 +1,86 @@
+(function () {
+	'use strict';
+
+	angular.module('angular.img', []).directive('httpSrc', ['$http', function ($http) {
+		return {
+		  restrict: 'A',
+		  scope: true,
+			link: function ($scope, elem, attrs) {
+				function revokeObjectURL() {
+					if ($scope.objectURL) {
+						URL.revokeObjectURL($scope.objectURL);
+					}
+				}
+
+				$scope.$watch('objectURL', function (objectURL) {
+					elem.attr('src', objectURL);
+				});
+
+				$scope.$on('$destroy', function () {
+					revokeObjectURL();
+				});
+
+				attrs.$observe('httpSrc', function (url) {
+					revokeObjectURL();
+
+					if(url && url.indexOf('data:') === 0) {
+						$scope.objectURL = url;
+					} else if(url) {
+						$http.get(url, { responseType: 'arraybuffer' })
+							.then(function (response) {
+								var blob = new Blob(
+									[ response.data ],
+									{ type: response.headers('Content-Type') }
+								);
+								$scope.objectURL = URL.createObjectURL(blob);
+							}, function error(err) {
+							  console.log(err);
+              });
+					}
+				});
+			}
+		};
+	}]);
+}());
+// (function() {
+//     'use strict';
+//
+//     angular.module('angular.img', []).directive('httpSrc', [
+//         '$http',
+//         function($http) {
+//             var directive = {
+//                 link: link,
+//                 restrict: 'A'
+//             };
+//             return directive;
+//
+//             function link(scope, element, attrs) {
+//                 var requestConfig = {
+//                     method: 'Get',
+//                     url: attrs.httpSrc,
+//                     responseType: 'arraybuffer',
+//                     cache: 'true'
+//                 };
+//
+//                 $http(requestConfig)
+//                     .then(function(res) {
+//                         var data = res.data;
+//                         var arr = new Uint8Array(data);
+//
+//                         var raw = '';
+//                         var i, j, subArray, chunk = 5000;
+//                         for (i = 0, j = arr.length; i < j; i += chunk) {
+//                             subArray = arr.subarray(i, i + chunk);
+//                             raw += String.fromCharCode.apply(null, subArray);
+//                         }
+//
+//                         var b64 = btoa(raw);
+//                         var minetype = res.headers('Content-Type');//image/png;
+//                         var img = "data:"+minetype+";base64," + b64;
+//                         attrs.$set('src', img);
+//                     });
+//             }
+//
+//         }
+//     ]);
+// }());
